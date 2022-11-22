ada52754e180c5f01883680b629fb6850739645c
FunGvey <99077969+FunGvey@users.noreply.github.com>
2022-11-22 20:21:32 +0300

    03

--- bin/guess-number.php
+++ bin/guess-number.php
@@ -1,11 +1,12 @@
 <?php
-    $autoLoadGit = __DIR__.'/../vendor/autoload.php';
-    $autoLoadPackgaist = __DIR__.'/../../../autoload.php';
 
-    if(file_exists($autoLoadGit)){
-        require_once($autoLoadGit);
-    } else {require_once($autoLoadPackgaist);}
+    $autoLoadGit = __DIR__ . '/../vendor/autoload.php';
+    $autoLoadPackgaist = __DIR__ . '/../../../autoload.php';
 
-    use function fungvey\guess_number\Controller\startGame;
-    startGame();
-?>
+if (file_exists($autoLoadGit)) {
+    require_once($autoLoadGit);
+} else {
+    require_once($autoLoadPackgaist);
+}
+    use function fungvey\guess_number\Controller\menu;
+    menu();
